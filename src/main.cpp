3e902bffd070fb5d5248748ff1e4666fc20edcfe
Vineet Bansal <vineetbansal@gmail.com>
2020-02-11 20:47:27 -0500

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <pybind11/pybind11.h>
+
+int add(int i, int j) {
+    return i + j;
+}
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(celia, m) {
+    m.attr("__name__") = "paula.celia";
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+
+        Some other explanation about the add function.
+    )pbdoc");
+
+    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
+        Subtract two numbers
+
+        Some other explanation about the subtract function.
+    )pbdoc");
+
+#ifdef VERSION_INFO
+    m.attr("__version__") = VERSION_INFO;
+#else
+    m.attr("__version__") = "dev";
+#endif
+}
