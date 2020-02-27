e2f76c4850a3a43ca9dc88a16b7538eb40c3c884
Vineet Bansal <vineetbansal@gmail.com>
2020-02-27 16:22:00 -0500

    Create build-wheels.sh

---
+++ build-wheels.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e -x
+
+# Collect the pythons
+pys=(/opt/python/*/bin)
+
+# Filter out Python 3.4
+pys=(${pys[@]//*34*/})
+
+# Compile wheels
+for PYBIN in "${pys[@]}"; do
+    "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/$package_name-*.whl; do
+    auditwheel repair "$whl" -w /io/wheelhouse/
+done
+
