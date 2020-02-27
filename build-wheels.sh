717dc80de00cae0fd0bf9f0d4badf651aa229f3b
Vineet Bansal <vineetbansal@gmail.com>
2020-02-27 16:56:42 -0500

    Update build-wheels.sh

--- build-wheels.sh
+++ build-wheels.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 set -e -x
 
-# Collect the pythons
-pys=(/opt/python/*/bin)
+pys=(/opt/python/cp37-*/bin)
 
-# Filter out Python 3.4
-pys=(${pys[@]//*34*/})
-
-# Compile wheels
 for PYBIN in "${pys[@]}"; do
     "${PYBIN}/pip" install -r /io/requirements.txt
+    "${PYBIN}/cmake" -B/io/src/paula/celia/build -S/io/src/paula/celia
+    "${PYBIN}/cmake" --build /io/src/paula/celia/build
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
 
-# Bundle external shared libraries into the wheels
 for whl in wheelhouse/$package_name-*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
-
