9fea5c93213829ff5898d54ed27e43f48f0845c2
Vineet Bansal <vineetbansal@protonmail.com>
2020-06-25 17:34:35 -0400

    dynamic version numbers for c submodule, specified through cmake

--- src/paula/celia/src/main.cpp
+++ src/paula/celia/src/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "hello.h"
+#include "version.h"
 
 int main(int argc, char *argv[])
 {
     Hello hello;
     std::cout << hello.greet() << std::endl;
     std::cout << "OMP thread id sum = " << omp_sum() << std::endl;
+    std::cout << "Version number = " << MY_VERSION << std::endl;
     return 0;
 }
